dceec96f2d6f214b309783c4e8b4773a4b0d710a
xkylz1 <fauzansaputra741@gmail.com>
2024-11-10 22:37:31 +0800

    feat: update hero and navbar

--- Binar Chapter 6/src/App.jsx
+++ Binar Chapter 6/src/App.jsx
@@ -4,47 +4,35 @@ import axios from "axios";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import NavbarWithStyling from "./components/NavbarWithStyling";
-import { HoverButton } from "./components/Elements/button/HoverButton";
-import { AboutView } from "./pages/AboutView";
-import { NotFoundView } from "./pages/NotFound";
-import { HomeView } from "./pages/HomeView";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { LoginPage } from "./pages/Login";
-import { RegisterPage } from "./pages/Register";
+import NavbarWithStyling from "./components/navbar/navbarWithStyling";
+// import Navbar from "./components/navbar/navbar";
+// import HoverButton from "./components/buttons/HoverButton";
 
-const router = createBrowserRouter([
+const people = [
   {
-    path: "/",
-    element: <HomeView />,
-    errorElement: <NotFoundView />,
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
-  {
-    path: "/about",
-    element: <AboutView />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/register",
-    element: <RegisterPage />,
-  },
-]);
+];
+
 function App() {
-  // store data secara state react
+  // store data secara state react nya
   const [shops, setShops] = useState([]);
-  const listMenu = ["Home", "About", "Logout"];
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  // fetch data => fetch/axios
+  const listMenu = ["Home", "About", "Logout"];
+
+  console.log(shops);
+
+  // fetch data => fetch / axios
   useEffect(() => {
     const fetchShops = async () => {
       setLoading(true);
       try {
-        const response = await axios.get("http://localhost:5000/api/v1/shops");
+        const response = await axios.get("http://localhost:3000/api/v1/shops");
         console.log(response);
 
         const data = response.data;
@@ -52,11 +40,9 @@ function App() {
           setShops(data.data.shops);
         } else {
           console.log(response);
-
           setError("error");
         }
       } catch (error) {
-        console.log(error);
         setError(error.message);
       } finally {
         setLoading(false);
@@ -65,12 +51,52 @@ function App() {
 
     fetchShops();
   }, []);
+
+  console.log(shops);
+
   return (
     <>
-      {loading && <p>loading</p>}
+      <div className="bg-white py-24 sm:py-32">
+        <div className="mx-auto grid max-w-7xl gap-20 px-6 lg:px-8 xl:grid-cols-3 tex">
+          <div className="max-w-xl">
+            <h2 className="text-pretty text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl">
+              Meet our leadership
+            </h2>
+            <p className="mt-6 text-lg/8 text-gray-600">
+              We’re a dynamic group of individuals who are passionate about what
+              we do and dedicated to delivering the best results for our
+              clients.
+            </p>
+          </div>
+          <ul
+            role="list"
+            className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2"
+          >
+            {people.map((person) => (
+              <li key={person.name}>
+                <div className="flex items-center gap-x-6">
+                  <img
+                    alt=""
+                    src={person.imageUrl}
+                    className="h-16 w-16 rounded-full"
+                  />
+                  <div>
+                    <h3 className="text-base/7 font-semibold tracking-tight text-gray-900">
+                      {person.name}
+                    </h3>
+                    <p className="text-sm/6 font-semibold text-indigo-600">
+                      {person.role}
+                    </p>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
 
-      <main className="text-center">
-        {/* <header className="flex justify-between p-4 bg-white shadow-md">
+      {/* header */}
+      <header className="flex justify-between p-4 bg-white shadow-md">
         <div className="flex items-center space-x-4">
           <h1 className="text-lg font-bold text-blue-800">Binar Car Rental</h1>
           <nav className="hidden md:flex space-x-4">
@@ -91,46 +117,41 @@ function App() {
         <button className="px-4 py-2 text-white bg-green-500 rounded-md">
           Register
         </button>
-      </header> */}
-
+      </header>
+      <main className="text-center">
+        {loading && <p> loading... </p>}
         {error && <p className="text-red-500">{error}</p>}
-        {!error && (
+        {!loading && !error && (
           <section className="max-w-6xl mx-auto mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {" "}
             {shops.map((shop, index) => (
               <div
                 key={index}
                 className="p-4 border rounded-md bg-white shadow-md"
               >
-                {" "}
                 <img
                   src={shop.products[0].images[0]}
                   alt={shop.products[0].name}
                   className="w-full h-40 object-cover mb-4"
                 />
-                <h3 style={{ color: "#000000" }} className="font-semibold">
-                  {shop.products[0].name}
-                </h3>
+                <h3 className="font-semibold">{shop.products[0].name}</h3>
                 <p className="text-green-500 font-bold">
-                  {shop.products[0].price}/ hari
-                </p>{" "}
+                  Rp{shop.products[0].price} / hari
+                </p>
                 <p className="text-gray-600 mt-2 text-sm">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </p>{" "}
-                <div className="flex items-center justify-between text-gray-500 text-sm mt-4">
-                  {" "}
-                  <span>4 orang</span> <span>Manual</span>{" "}
-                  <span>Tahun 2020</span>{" "}
-                </div>{" "}
+                </p>
+                <div className="flex items-center justify-around text-gray-500 text-sm mt-4">
+                  <span>4 orang</span>
+                  <span>Manual</span>
+                  <span>Tahun 2020</span>
+                </div>
                 <button className="w-full px-4 py-2 mt-4 text-white bg-green-500 rounded-md">
                   Pilih Mobil
-                </button>{" "}
+                </button>
               </div>
-            ))}{" "}
+            ))}
           </section>
         )}
-
-        {/* <RouterProvider router={router} />; */}
       </main>
     </>
   );
