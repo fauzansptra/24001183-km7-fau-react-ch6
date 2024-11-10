c82545b552ccf6121b1690d63bb363bf5abe0a33
xkylz1 <fauzansaputra741@gmail.com>
2024-11-11 04:34:48 +0800

    feat: update content for rating and shop name

--- Binar Chapter 6/src/components/shopList/ShopList.jsx
+++ Binar Chapter 6/src/components/shopList/ShopList.jsx
@@ -21,9 +21,8 @@ function ShopList({ shops, loading, error }) {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </p>
           <div className="flex items-center justify-around text-gray-500 text-sm mt-4">
-            <span>4 orang</span>
-            <span>Manual</span>
-            <span>Tahun 2020</span>
+            <span>5 Star</span>
+            <span>{shop.name}</span>
           </div>
           <button className="w-full px-4 py-2 mt-4 text-white bg-green-500 rounded-md">
             Pilih Mobil
